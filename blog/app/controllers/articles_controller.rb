2a1f12d456795370bd27ec728c062219ab2dd1ad
Martient <martient.pro@gmail.com>
2018-05-09 18:01:32 +0200

    Project BLOG: add remove article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -15,6 +15,13 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
 	def create
 		@article = Article.new(article_params)
 

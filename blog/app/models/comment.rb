fab3b2cfdc2af11323156ccca331489e69c9146d
Martient <martient.pro@gmail.com>
2018-05-11 13:39:22 +0200

    Project BLOG: Add comment section in show page.

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,11 +1,15 @@
-class CreateComments < ActiveRecord::Migration[5.0]
-	def change
-		create_table :comments to |t|
-			t.string :commenter
-			t.text :body
-			t.references :article, foreign_key: true
+class Comment < ApplicationRecord
+  belongs_to :article
+end
 
-			t.timestamps
-		end
-	end
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :article, foreign_key: true
+
+      t.timestamps
+    end
+  end
 end
